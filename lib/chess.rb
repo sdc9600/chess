e4f9310d8bde45e631cd54d2de3a53140c3073fd
sdc9600 <149299980+sdc9600@users.noreply.github.com>
2024-09-30 23:53:49 +1000

    Legal moves check function in progress

--- lib/chess.rb
+++ lib/chess.rb
@@ -18,8 +18,8 @@ class Chess
     puts "#{@gameboard[7]}\n#{gameboard[6]}\n#{@gameboard[5]}\n#{gameboard[4]}\n#{@gameboard[3]}\n#{gameboard[2]}\n#{gameboard[1]}\n#{gameboard[0]}}"
   end
 
-  def make_move
-    puts "Enter the square where you piece is currently located and the square where you wish to move to as a 4-digit string e.g. 'e2e4'"
+  def player_input_move
+    puts "Enter the square where your piece is currently located and the square where you wish to move to as a 4-digit string e.g. 'e2e4'"
     input = gets.chomp
     validate_move(input)
   end
@@ -29,7 +29,7 @@ class Chess
     move = move.chars
     move.each do |square|
      # binding.pry
-      move[x] = (square.ord - 98) if square.to_i == 0
+      move[x] = (square.ord - 97) if square.to_i == 0
       move[x] = move[x].to_i - 1 if square.to_i != 0
       x+= 1
     end 
@@ -39,15 +39,31 @@ class Chess
     p piece_type
     p starting_square
     p destination_square
+    legal_moves_check(starting_square, destination_square, piece_type)
   end
 
-  def legal_moves_array(starting_square, piece_type)
+  def legal_moves_check(starting_square, destination_square, piece_type)
+    inbetween_squares_array = []
+    x = 1
+    return false if starting_square == destination_square # The piece needs to actually move, passing is not valid
+    return false if destination_square.any? {|element| element > 7 || element < 0} #Out of bounds check, a piece can't leave the gameboard
+    if starting_square[0] < destination_square[0] && starting_square[1] == destination_square[1]
+      until starting_square[0] + x == destination_square[0] do
+        inbetween_squares_array.append([starting_square[0] + x, starting_square[1]])
+        x += 1
+      end
+    elsif starting_square[0] > destination_square[0] && starting_square[1] == destination_square[1]
+      until starting_square[0] + x == destination_square[0] do
+        inbetween_squares_array.append([starting_square[0] - x, starting_square[1]])
+        x += 1
+      end
+    end  # A check if there is any piece being passed over on the trip to the destination square (Ignored for knights and kings)
     if piece_type == "wP"
-      legal_moves_array = 
+    end
+    p inbetween_squares_array
   end
 end
 
 game = Chess.new
 game.starting_posiiton
-game.display_chessboard
-game.validate_move("e2e4")
+game.validate_move("e2a2")
